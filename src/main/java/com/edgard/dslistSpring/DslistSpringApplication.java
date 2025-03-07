625985b7e285e4e3a52cafcaadabb03d43081c70
Edgard Ribeiro <ribeiro.edgard@gmail.com>
2025-03-07 09:46:24 -0300

    Project created

---
+++ src/main/java/com/edgard/dslistSpring/DslistSpringApplication.java
@@ -0,0 +1,13 @@
+package com.edgard.dslistSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DslistSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DslistSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edgard/dslistSpring/DslistSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edgard.dslistSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
