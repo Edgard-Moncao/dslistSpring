e7dacd3d8f7e9ace065b8acb2844aa6681650192
Edgard Ribeiro <ribeiro.edgard@gmail.com>
2025-03-14 00:11:17 -0300

    GET game by id

--- src/main/java/com/edgard/dslistSpring/services/GameService.java
+++ src/main/java/com/edgard/dslistSpring/services/GameService.java
@@ -1,10 +1,13 @@
 package com.edgard.dslistSpring.services;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.edgard.dslistSpring.dto.GameDTO;
 import com.edgard.dslistSpring.dto.GameMinDTO;
 import com.edgard.dslistSpring.entities.Game;
 import com.edgard.dslistSpring.repositories.GameRepository;
@@ -15,9 +18,16 @@ public class GameService {
 	@Autowired
 	private GameRepository gameRepository;
 	
+	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll(){
 		List<Game> result = gameRepository.findAll();
 		return result.stream().map(x -> new GameMinDTO(x)).toList();		
+	}
+	
+	@Transactional(readOnly = true)
+	public GameDTO findById(Long id) {
+		Game result = gameRepository.findById(id).get();		
+		return new GameDTO(result);
 	}
 
 }
